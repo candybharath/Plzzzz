4cf890bc51c245b022caf5b243c4418da8afa1e7
narutooxox <112558957+narutooxox@users.noreply.github.com>
2023-04-09 00:06:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/skalight/Test.git /RolexTG
+  git clone https://github.com/narutooxox/RolexTG.git /RolexTG
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RolexTG
